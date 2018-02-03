445997e2c393a22d105fbe9c48c9ee507920fcca
tkaminski <tadeuszkaminski21cn@gmail.com>
2018-02-03 18:20:47 +0100

    Ten commit jest dodany z srodiska intelij

--- src/com/company/calcengine/Main.java
+++ src/com/company/calcengine/Main.java
@@ -3,27 +3,43 @@ package com.company.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-	    double val1 = 100.0;
-        double val2 = 50.0;
-        double result;
-        char opCode = 'd';
+        double[] leftVals = {100, 25, 225, 11};
+        double[] rightVals = {50, 92, 17, 3};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
 
-        if(opCode == 'a'){
-            result = val1 + val2;
-        } else if(opCode == 's'){
-            result = val1 - val2;
-        } else if(opCode == 'd'){
-            if(val2 != 0){
-                result = val1/val2;
-            } else {
-                result = 0;
+
+//	    double val1 = 100.0;
+//      double val2 = 50.0;
+//      double result;
+//      char opCode = 'd';
+
+        for (int i=0; i<opCodes.length; i++){
+            switch(opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'd':
+                    if(rightVals[i] != 0){
+                        results[i] = leftVals[i]/rightVals[i];
+                    } else {
+                        results[i] = 0;
+                    } break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                default:
+                    System.out.println("Error - invalid opCode");
+                    results[i] = 0.0;
+                    break;
             }
-        } else if (opCode == 'm'){ //zakomentowana linia na github.com 
-            result = val1 * val2;
-        } else {
-            result = 0.0;
         }
-
-        System.out.println(result);
+        for (double theResult : results){
+            System.out.print("result = ");
+            System.out.println(theResult);
+        }
     }
 }
