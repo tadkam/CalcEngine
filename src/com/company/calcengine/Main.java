03a64b4c4888859ac95cb2b29492dc9847175f56
tkaminski <tadeuszkaminski21cn@gmail.com>
2018-02-03 16:13:03 +0100

    Pierwsza wersja kalkulatora

---
+++ src/com/company/calcengine/Main.java
@@ -0,0 +1,29 @@
+package com.company.calcengine;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double val1 = 100.0;
+        double val2 = 50.0;
+        double result;
+        char opCode = 'd';
+
+        if(opCode == 'a'){
+            result = val1 + val2;
+        } else if(opCode == 's'){
+            result = val1 - val2;
+        } else if(opCode == 'd'){
+            if(val2 != 0){
+                result = val1/val2;
+            } else {
+                result = 0;
+            }
+        } else if (opCode == 'm'){
+            result = val1 * val2;
+        } else {
+            result = 0.0;
+        }
+
+        System.out.println(result);
+    }
+}
