466790259795c5d2425f21a4cc961b4829e961d0
tadeusz.kaminski <tadeusz.kaminski@accenture.com>
2018-02-05 10:03:18 +0100

    Dodanie clasy MathEquation i wszystkich związanych z tym przeróbek.

--- src/com/company/calcengine/Main.java
+++ src/com/company/calcengine/Main.java
@@ -3,10 +3,17 @@ package com.company.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-        double[] leftVals = {100, 25, 225, 11};
-        double[] rightVals = {50, 92, 17, 3};
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
+//        double[] leftVals = {100, 25, 225, 11};
+//        double[] rightVals = {50, 92, 17, 3};
+//        char[] opCodes = {'d', 'a', 's', 'm'};
+//        double[] results = new double[opCodes.length];
+
+
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100, 50, 'd');
+        equations[1] = create(25, 92, 'a');
+        equations[2] = create(225, 17, 's');
+        equations[3] = create(11, 3, 'm');
 
 
 //	    double val1 = 100.0;
@@ -14,32 +21,24 @@ public class Main {
 //      double result;
 //      char opCode = 'd';
 
-        for (int i=0; i<opCodes.length; i++){
-            switch(opCodes[i]) {
-                case 'a':
-                    results[i] = leftVals[i] + rightVals[i];
-                    break;
-                case 's':
-                    results[i] = leftVals[i] - rightVals[i];
-                    break;
-                case 'd':
-                    if(rightVals[i] != 0){
-                        results[i] = leftVals[i]/rightVals[i];
-                    } else {
-                        results[i] = 0;
-                    } break;
-                case 'm':
-                    results[i] = leftVals[i] * rightVals[i];
-                    break;
-                default:
-                    System.out.println("Error - invalid opCode");
-                    results[i] = 0.0;
-                    break;
-            }
-        }
-        for (double theResult : results){
+
+        for (MathEquation equation: equations){
+            equation.execute();
             System.out.print("result = ");
-            System.out.println(theResult);
+            System.out.println(equation.result);
         }
     }
+
+    public static MathEquation create(double leftVal, double rightVal, char opCode ){
+        MathEquation equation = new MathEquation();
+
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+
+        return equation;
+    }
+
+
+
 }
