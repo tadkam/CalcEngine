86a06b8a193a933774e913dfa18a192414e8a33b
tadeusz.kaminski <tadeusz.kaminski@accenture.com>
2018-02-05 11:54:09 +0100

    Usunięcie z funkcji main metody create. Dodanie do klasy MainEquation konstruktorów.2

--- src/com/company/calcengine/Main.java
+++ src/com/company/calcengine/Main.java
@@ -10,10 +10,10 @@ public class Main {
 
 
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100, 50, 'd');
-        equations[1] = create(25, 92, 'a');
-        equations[2] = create(225, 17, 's');
-        equations[3] = create(11, 3, 'm');
+        equations[0] = new MathEquation('d',100, 50);
+        equations[1] = new MathEquation('a', 25, 92);
+        equations[2] = new MathEquation('s', 225, 17);
+        equations[3] = new MathEquation('m', 11, 3);
 
 
 //	    double val1 = 100.0;
@@ -25,20 +25,9 @@ public class Main {
         for (MathEquation equation: equations){
             equation.execute();
             System.out.print("result = ");
-            System.out.println(equation.result);
+            System.out.println(equation.getResult());
         }
     }
-
-    public static MathEquation create(double leftVal, double rightVal, char opCode ){
-        MathEquation equation = new MathEquation();
-
-        equation.leftVal = leftVal;
-        equation.rightVal = rightVal;
-        equation.opCode = opCode;
-
-        return equation;
-    }
-
 
 
 }
